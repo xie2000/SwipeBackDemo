96f17d641031c67f86f022f14098b39bd1aaa5a8
xie2000 <xiechengfa2000@163.com>
2015-05-17 23:50:38 +0800

    aa

---
+++ src/com/example/swipebackdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.swipebackdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button btn = (Button) findViewById(R.id.btn);
+		btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MainActivity.this,
+						SecondActivity.class));
+			}
+		});
+	}
+}
---
+++ src/com/example/swipebackdemo/SecondActivity.java
@@ -0,0 +1,12 @@
+package com.example.swipebackdemo;
+
+import android.os.Bundle;
+
+public class SecondActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second_activity_layout);
+	}
+}
